cb96d5c66634ee46ee5a15cb12a79ffe596c48e5
lanang <lananglanusaputera@gmail.com>
2025-01-19 11:38:50 +0800

    lanang

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const response = NextResponse.next();
 
-  response.headers.set('Access-Control-Allow-Origin', '*'); // Ganti * dengan domain tertentu jika perlu
+  response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
